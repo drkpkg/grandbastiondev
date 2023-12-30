c439e614655c9969a16cd298e3533ed962a52d44
Felix Daniel Coca Calvimontes <daniel.uremix@gmail.com>
2023-12-30 00:28:20 -0400

    Changes to api

--- src/pages/api/profile.js
+++ src/pages/api/profile.js
@@ -1,14 +1,16 @@
+import { Octokit, App } from "octokit";
 export async function getProfile(params, request) {
     // get information from my github profile
     try {
-        const githubPersonalToken = process.env.G_PERSONAL_TOKEN;
-        const profileResponse = await fetch("https://api.github.com/users/drkpkg", {
-            headers: {
-                Authorization: `token ${githubPersonalToken}`,
-            },
-        });
+        const profileResponse = await fetch("https://api.github.com/users/drkpkg");
         const jsonResponse = await profileResponse.json();
         return new Response(JSON.stringify(jsonResponse), {
+            headers: {
+                "content-type": "application/json;charset=UTF-8",
+            },
+            status: 200,
+        });
+        return new Response(JSON.stringify(data), {
             headers: {
                 "content-type": "application/json;charset=UTF-8",
             },
